026c80bf8b5167c939dd9e505d8336a7892afd91
MoskalkovaAlina <moskalkovaalina@gmail.com>
2024-12-24 02:20:24 +0300

    task2

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const sliderContainer = document.querySelector('.slider-container');
-const slides = document.querySelectorAll('.slide');
-        const nextButton = document.getElementById('next');
-        const prevButton = document.getElementById('prev');
+$(document).ready(function(){
+    let currentIndex = 0;
+    const slides = $('.slider-box');
+    const totalSlides = slides.length;
 
-        let currentSlide = 0;
+    // Инициализация: показываем первый слайд
+    function showSlide(index) {
+        slides.removeClass('active');
+        slides.eq(index).addClass('active');
+    }
 
-        nextButton.addEventListener('click', () => {
-            slides[currentSlide].classList.remove('active');
-            currentSlide = (currentSlide + 1) % slides.length;
-            slides[currentSlide].classList.add('active');
-        });
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        showSlide(currentIndex);
+    }
 
-        prevButton.addEventListener('click', () => {
-            slides[currentSlide].classList.remove('active');
-            currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-            slides[currentSlide].classList.add('active');
-        });
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+        showSlide(currentIndex);
+    }
+
+    // Кнопки навигации
+    $('#right-arrow').click(function(){
+        nextSlide();
+    });
+
+    $('#left-arrow').click(function(){
+        prevSlide();
+    });
+
+    // Инициализация слайдера
+    showSlide(currentIndex);
+});
